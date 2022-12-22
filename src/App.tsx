784d57fd5952466c6b0c747052bcd57fd7bcb3e7
Smartsam125 <samsonmujabi125@gmail.com>
2022-12-22 20:50:08 +0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonApp,IonIcon,IonRouterOutlet,IonTabBar,IonTabButton,IonTabs } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { bluetoothOutline, settingsOutline } from 'ionicons/icons';
+import { Terminal, Settings } from './pages'
+
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route path="/ble-terminal" component={Terminal} />
+          <Route path="/ble-settings" component={Settings} />
+          <Route path="/" render={() => <Redirect to="/ble-terminal" />} exact={true} />
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="BLE Terminal" href="/ble-terminal">
+            <IonIcon icon={bluetoothOutline} />
+          </IonTabButton>
+          <IonTabButton tab="BLE Settings" href="/ble-settings">
+            <IonIcon icon={settingsOutline} />
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
